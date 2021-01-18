4c2f8d0560bce26a1280ec01103f6248e324a652
99002780 <72479185+99002780@users.noreply.github.com>
2021-01-18 18:34:35 +0530

    Add files via upload

---
+++ react-material/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import AppRouter from "./components/RouterComponent";
+import NavBar from "./components/Navbar";
+import Container from '@material-ui/core/Container';
+
+function App() {
+  return (
+      <div>
+          <NavBar/>
+          <Container>
+                <AppRouter/>
+          </Container>
+      </div>
+  );
+}
+
+export default App;
